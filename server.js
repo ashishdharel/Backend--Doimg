b30b68644772d2698ada2202c83b9e3d876cdcef
Ashish Dharel <ashishdharel>
2024-07-15 10:58:07 +0545

    abin new

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-    host: '13.229.115.49',
+    host: 'localhost',
     port: '3306',
     user: 'helloashish',
     password: 'password',
